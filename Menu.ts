cb44284e146e801b7787ee03f112f374dc60c866
Alice Tolosa <aliceoliveira81@outlook.com>
2026-06-22 09:03:34 -0300

    instalação das bibliotecas e criação da Classe Menu

---
+++ Menu.ts
@@ -0,0 +1,25 @@
+import readlineSync from 'readline-sync'; //importando a biblioteca readline-sync para receber dados do usuário
+export function main() {
+    
+    let opcao: number ; //variavel numero que receberar o numero do menu que o usuário digitar
+    while(true) {  // Laço de Repetição while, que vai repetir o menu até que o usuário digite a opção de sair/ true para que seja infinito
+    
+        console.log("***********************************")
+        console.log("           BIBLIOTECA TECH         ")
+        console.log("***********************************")
+        console.log("       Menu de Opções:             "); //imprimindo no console o menu de opções
+        console.log("  1 - Cadastrar Livro              ");
+        console.log("  2 - Listar todos os Livros       ");
+        console.log("  3 - Buscar Livro ID              ");
+        console.log("  4 - Atualizar dados doLivro      ");
+        console.log("  5 - Deletar Livro                ");
+        console.log("  6 - Vender Livro                 ");
+        console.log("  7- Adicionar ao Estoque          ");
+        console.log("  0 - Sair                         ");
+
+
+
+
+
+    }
+}
